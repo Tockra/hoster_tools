2bf65899d62eed2497beffcc367380841c424f86
Tim <tim.tannert@tu-dortmund.de>
2020-01-31 16:56:53 +0100

    Changed to blocking impl

--- src/hosters/onyxhosting.rs
+++ src/hosters/onyxhosting.rs
@@ -1,30 +1,32 @@
-use reqwest::ClientBuilder;
+use reqwest::blocking::ClientBuilder;
 
 pub struct DNSManager {
     login_form: [(&'static str,&'static str);3]
 }
 
 impl DNSManager {
-    const LOGIN_URL: &'static str = "https://onyxhosting.de/dologin.php";
+    const LOGIN_URL: &'static str = "https://httpbin.org/anything"; //"https://onyxhosting.de/dologin.php";
 
     pub fn new(username: &'static str, password: &'static str) -> Result<Self, String> {
         
         Ok(Self {
-            login_form: [("token","30d57bc7df699adbe7809bf97293c92bc7fe9d5c"), ("username",username),("password",password)]
+            login_form: [("token","bd13711ac7c20c4851b6879460b27f02f5101596"), ("username",username),("password",password)]
         })
     }
 
-    pub async fn check_login(&self) -> Result<(), String> {
-        let client = ClientBuilder::new().cookie_store(true).build().unwrap();
+    pub fn check_login(&self) -> Result<(), String> {
+        let client = ClientBuilder::new().cookie_store(true).user_agent("curl/7.58.0").build().unwrap();
         println!("{:?}", client.post(Self::LOGIN_URL)
             .form(&self.login_form));
         println!("{:?}", self.login_form);
         let response = client.post(Self::LOGIN_URL)
             .form(&self.login_form)
             .send()
-            .await.map_err(|_|format!("Probleme bei Verbindungsaufbau mit {}", Self::LOGIN_URL))?;
-            
-        println!("{:?}", response.url().as_str().contains("incorrect=true"));
+            .map_err(|_|format!("Probleme bei Verbindungsaufbau mit {}", Self::LOGIN_URL))?;
+        println!("{}", response.text().unwrap());
+        //println!("{:?}", response.url().as_str());
+        
+        //println!("{:?}", response.url().as_str().contains("incorrect=true"));
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use hosters::onyxhosting::DNSManager;
 
-#[tokio::main]
-async fn main() {
-    let dns_manager = DNSManager::new("tim.tannert@tu-dormund.de", "debug").unwrap();
+fn main() {
+    let dns_manager = DNSManager::new("", "").unwrap();
 
-    dns_manager.check_login().await;
+    dns_manager.check_login();
 
 }
 
