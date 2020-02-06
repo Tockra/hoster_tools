5824dac4965a24a51652efd72597cf417d1ee5ed
Tim <tim.tannert@tu-dortmund.de>
2020-02-06 12:03:46 +0100

    Post-Prozess hinzugefügt

--- src/hosters/onyxhosting.rs
+++ src/hosters/onyxhosting.rs
@@ -92,6 +92,8 @@ impl DNSManager {
     const CLIENTAREA_URL: &'static str = "https://onyxhosting.de/clientarea.php";
     const DOMAIN_URL: &'static str = "https://onyxhosting.de/clientarea.php?action=domains";
     const DOMAINDNS_URL: &'static str = "https://onyxhosting.de/clientarea.php?action=domaindns&domainid=";
+    const DNSPOST_URL: &'static str = "https://onyxhosting.de/clientarea.php?action=domaindns";
+    
 
     pub fn new(username: &str, password: &str) -> Result<Self, LoginError> {
         let client = ClientBuilder::new().cookie_store(true).build().unwrap();
@@ -110,7 +112,7 @@ impl DNSManager {
     /// trys to login with the username and password given to the constructor
     fn login(&self) -> Result<(),LoginError> {
         // requests a token
-        let token = self.get_token()?;
+        let token = self.get_token(Self::CLIENTAREA_URL)?;
 
         // Builds the post form.
         let login_form: [(String,&String);4] = [(String::from("token"), &token), (String::from("username"), &self.username), (String::from("password"), &self.password), (String::from("rememberme"),&String::from("on"))];
@@ -129,18 +131,18 @@ impl DNSManager {
 
     /// This method returns the token String 
     /// catched from `CLIENTAREA_URL`. If something fails it returns a corresponding LoginError.
-    fn get_token(&self) -> Result<String, LoginError> {
-        let response = self.client.get(Self::CLIENTAREA_URL).send().map_err(|_| LoginError::HTTPConnectionError(String::from(Self::CLIENTAREA_URL)))?.text().map_err(|_| LoginError::HTTPConnectionError(String::from(Self::CLIENTAREA_URL)))?;
+    fn get_token(&self, token_url: &str) -> Result<String, LoginError> {
+        let response = self.client.get(token_url).send().map_err(|_| LoginError::HTTPConnectionError(String::from(token_url)))?.text().map_err(|_| LoginError::HTTPConnectionError(String::from(token_url)))?;
         
         let mut token_lines = response.lines().filter(|x| x.contains("var csrfToken ="));
 
         match token_lines.nth(0) {
-            None => Err(LoginError::UnknownParseError(format!("There wasn't at least one line with 'var csrfToken =' in the response from {}", Self::CLIENTAREA_URL))),
+            None => Err(LoginError::UnknownParseError(format!("There wasn't at least one line with 'var csrfToken =' in the response from {}", token_url))),
             Some(line) => 
                 Ok(String::from(line
                             .split("'")
                             .nth(1)
-                            .map_or(Err(LoginError::UnknownParseError(format!("In the response from {} aren't \"'\" after the \"var csrfToken\" part", Self::CLIENTAREA_URL))),|x| Ok(x))?
+                            .map_or(Err(LoginError::UnknownParseError(format!("In the response from {} aren't \"'\" after the \"var csrfToken\" part", token_url))),|x| Ok(x))?
                 ))
                 
         }
@@ -174,10 +176,12 @@ impl DNSManager {
         Err(LoginError::UnknownParseError(format!("Unexspected structure of the response of {}", Self::DOMAIN_URL)))
     }
 
-    fn get_current_records(&self, domain: &str) -> Result<Vec<Record>, LoginError> {
+    fn get_current_records(&self, domain: &str) -> Result<(String, String, Vec<Record>), LoginError> {
         let domain_id = self.get_domain_id(domain)?;
 
-        let document = Document::from(self.client.get(format!("{}{}", Self::DOMAINDNS_URL, domain_id).as_str()).send().unwrap().text().unwrap().as_str());
+        let document = Document::from(self.client.get(format!("{}{}", Self::DOMAINDNS_URL, domain_id).as_str()).send().map_err(|_| LoginError::HTTPConnectionError(String::from(format!("{}{}", Self::DOMAINDNS_URL, domain_id))))?.text().map_err(|_| LoginError::HTTPConnectionError(String::from(format!("{}{}", Self::DOMAINDNS_URL, domain_id))))?.as_str());
+        let token = self.get_token(format!("{}{}", Self::DOMAINDNS_URL, domain_id).as_str())?;
+
 
         let domain_info_tables = document.find(Name("table")).collect::<Vec<_>>();
         
@@ -204,7 +208,7 @@ impl DNSManager {
 
             record_list.push(builder.build()?);
         }
-        Ok(record_list)
+        Ok((token, domain_id, record_list))
     }
 
     /// Checks the current login status. If the login run out this method logins again. 
@@ -241,7 +245,7 @@ impl DNSManager {
                         priority: String::from("N/A"),
             };
 
-        let mut current_records = self.get_current_records(domain)?;
+        let (token, domain_id, mut current_records) = self.get_current_records(domain)?;
 
         if current_records.contains(&new_record) { 
             for i in 0..current_records.len() {
@@ -253,14 +257,35 @@ impl DNSManager {
             current_records.push(new_record);
         }
 
-        Ok(self.push_record_list(current_records)?)
+        Ok(self.push_record_list(token, domain_id, current_records)?)
     }
 
     /// This method pushs the Records in `records` to the dns server
-    fn push_record_list(&self, records: Vec<Record>) -> Result<(),LoginError> {
-        for record in records {
-            println!("Record: {:?}", record);
+    fn push_record_list(&self, token: String, domain_id: String, records: Vec<Record>) -> Result<(),LoginError> {
+        let dns_form = Self::build_form(token,domain_id,records);
+
+        let response = self.client.post(Self::DNSPOST_URL)
+            .form(&dns_form)
+            .send()
+            .map_err(|_| LoginError::HTTPConnectionError(String::from(Self::DNSPOST_URL)))?;
+
+        if response.url().as_str().contains("incorrect=true") {
+            Err(LoginError::WrongLoginData)
+        } else {
+            Ok(())
         }
-        unimplemented!();
+    }
+
+    fn build_form(token: String, domain_id: String, records: Vec<Record>) -> Box<[(String, String)]> {
+        let mut form = vec![("token", token.as_str()), ("sub", "save"), ("domainid", domain_id.as_str())];
+
+        for record in records.iter() {
+            form.push(("dnsrecid[]",record.id.as_str()));
+            form.push(("dnsrecordhost[]",record.host.as_str()));
+            form.push(("dnsrecordtype[]",record.rtype.as_str()));
+            form.push(("dnsrecordaddress[]",record.address.as_str()));
+            form.push(("dnsrecordpriority[]",record.priority.as_str()));
+        }
+        form.into_iter().map(|(x,y)| (String::from(x),String::from(y))).collect::<Vec<_>>().into_boxed_slice()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use hosters::onyxhosting::DNSManager;
 
 fn main() {
-    let dns_manager = DNSManager::new("", "");
+    let dns_manager = DNSManager::new("tim.tannert@tu-dortmund.de", "pw");
 
     match dns_manager {
         Err(e) => println!("LoginProblem {:?}", e),
         Ok(x) => {
-           x.add_dns_record("", "mail2", "A", "").unwrap();
+           x.add_dns_record("oh12.de", "penis", "A", "1.2.3.4.5").unwrap();
         },
     }
 
